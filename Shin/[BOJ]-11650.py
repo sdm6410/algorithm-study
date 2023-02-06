894fe56899292ea9c371fec3824bfe6add1373c8
sdm6410 <sdm6410@naver.com>
2023-02-06 14:32:10 +0900

    [BOJ] 좌표 정렬하기 / 실버5 / 8분

---
+++ Shin/[BOJ]-11650.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+arr = []
+for _ in range(N):
+    x, y = map(int, input().split())
+    arr.append([x,y])
+arr = sorted(arr)
+for i, j in arr:
+    print(i, j)
