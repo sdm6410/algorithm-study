6cd02312b90658151a9ec3422ce2fbccd17ef0d8
sdm6410 <sdm6410@naver.com>
2023-03-23 10:27:37 +0900

    [BOJ] N번째 큰 수 /실버 2/ 33분

---
+++ Shin/[BOJ]-2075.py
@@ -0,0 +1,11 @@
+import heapq
+
+hq = []
+n = int(input())
+for _ in range(n):
+    for i in map(int, input().split()):
+        if(len(hq) >= n):
+            heapq.heappushpop(hq, i)
+        else:
+            heapq.heappush(hq, i)
+print(heapq.heappop(hq))
