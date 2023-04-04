cbece4d4a3efbbe63a6aa7428067c700eaf519d7
sdm6410 <sdm6410@naver.com>
2023-04-04 13:58:45 +0900

    [BOJ] 보물섬 / 골드 5 / 52분

---
+++ Shin/[BOJ]-2589.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+dx = [1,-1,0,0]
+dy = [0,0,1,-1]
+
+def bfs(x, y):
+    q.append([x, y])
+    c = [[0] * m for _ in range(n)]
+    c[x][y] = 1
+    num = 0
+    while(q):
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if(0 <= nx < n and 0 <= ny < m):
+                if(space[nx][ny] == 'L' and c[nx][ny] == 0):
+                    c[nx][ny]= c[x][y] + 1
+                    num = max(num, c[nx][ny])
+                    q.append([nx, ny])
+    print(c)
+    return num-1
+
+n, m = map(int,input().split())
+space = []
+for _ in range(n):
+    space.append(list(input()))
+
+q = deque()
+cnt = 0
+for i in range(n):
+    for j in range(m):
+        if(space[i][j] == 'L'):
+            cnt = max(cnt, bfs(i, j))
+
+print(cnt)
