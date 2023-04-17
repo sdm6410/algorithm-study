010c1cbd33e2821d1e42be56e920aa847a0df4a6
sdm6410 <sdm6410@naver.com>
2023-04-18 00:08:57 +0900

    [BOJ] 알고리즘 수업 - 피보나치 수 1 / 브론즈 1 / 27분

---
+++ Shin/[BOJ]-24416.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+def fib(n):
+    if(n == 1 or n==2):
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+
+def fibo(n):
+    f = [0] * (n + 1)
+    f[1] = 1
+    f[2] = 1
+    cnt = 0
+    for i in range(3, n+1):
+        cnt += 1
+        f[i] = f[i-1] + f[i-2]
+    return cnt
+print(fib(n),fibo(n))
