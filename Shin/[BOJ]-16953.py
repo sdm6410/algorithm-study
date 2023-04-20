dead641ccae106f35ce15c2ac775fdf145ce24ee
sdm6410 <sdm6410@naver.com>
2023-04-20 22:49:15 +0900

    [BOJ] A → B / 실버 2 / 30분

---
+++ Shin/[BOJ]-16953.py
@@ -0,0 +1,14 @@
+a,b = map(int, input().split())
+count = 1
+while(b != a):
+    count += 1
+    temp = b
+    if(b % 10 == 1):
+        b = b // 10
+    elif(b % 2 == 0):
+        b = b // 2
+    if(temp == b):
+        print(-1)
+        break
+else:
+    print(count)
